6047b5e1c2051f0dbe8e3aa1ba917c9c0b08ebac
Joao Paulo Ramos <joao.oramos11@gmail.com>
2021-07-28 12:51:01 -0300

    Added a generic settings load for hosts

--- kerlescan/config.py
+++ kerlescan/config.py
@@ -12,38 +12,42 @@ def str_to_bool(s):
         raise ValueError("Valid string argument expected")
     raise ValueError("Unable to determine boolean value from given string argument")
 
-if isClowderEnabled():
-    print("NEW ClowderConfig: ", LoadedConfig)
+def load_hosts_setting(env_name, clowder_endpoint, default):
+    if isClowderEnabled():
+        cfg = LoadedConfig
 
-    cfg = LoadedConfig
+        final_endpoint = ""
+        for endpoint in cfg.endpoints:
+            if endpoint.app == clowder_endpoint:
+                final_endpoint = f"http://{endpoint.hostname}:{endpoint.port}"
+                return final_endpoint
 
-    rbac_svc_hostname = ""
-    for endpoint in cfg.endpoints:
-        if endpoint.app == "rbac":
-            rbac_svc_hostname = f"http://{endpoint.hostname}:{endpoint.port}"
-            break
-else:
+        if final_endpoint == "":
+            return default
+    else:
+        return os.getenv(env_name, default)
 
-    log_level = os.getenv("LOG_LEVEL", "INFO")
-    inventory_svc_hostname = os.getenv(
-        "INVENTORY_SVC_URL", "http://inventory_svc_url_is_not_set"
-    )
-    baseline_svc_hostname = os.getenv(
-        "BASELINE_SVC_URL", "http://baseline_svc_url_is_not_set"
-    )
-    rbac_svc_hostname = os.getenv("RBAC_SVC_URL", "http://rbac_svc_url_is_not_set")
-    hsp_svc_hostname = os.getenv("HSP_SVC_URL", "http://hsp_svc_url_is_not_set")
-    drift_svc_hostname = os.getenv("DRIFT_SVC_URL", "http://drift_svc_url_is_not_set")
+inventory_svc_hostname = load_hosts_setting("INVENTORY_SVC_URL", 'host-inventory', "http://inventory_svc_url_is_not_set")
 
-    drift_shared_secret = os.getenv("DRIFT_SHARED_SECRET", None)
+baseline_svc_hostname = load_hosts_setting("BASELINE_SVC_URL", 'baseline', "http://baseline_svc_url_is_not_set")
 
-    prometheus_multiproc_dir = os.getenv("prometheus_multiproc_dir", None)
+rbac_svc_hostname = load_hosts_setting("RBAC_SVC_URL", 'rbac', "http://rbac_svc_url_is_not_set")
 
-    path_prefix = os.getenv("PATH_PREFIX", "/api/")
+hsp_svc_hostname = load_hosts_setting("HSP_SVC_URL", 'hsp', "http://hsp_svc_url_is_not_set")
 
-    enable_rbac = str_to_bool(os.getenv("ENABLE_RBAC", "True"))
+drift_svc_hostname = load_hosts_setting("DRIFT_SVC_URL", 'drift', "http://drift_svc_url_is_not_set")
 
-    enable_smart_mgmt_check = str_to_bool(
-        os.getenv("ENABLE_SMART_MANAGEMENT_ENTITLEMENT_CHECK", "False"))
+log_level = os.getenv("LOG_LEVEL", "INFO")
+
+drift_shared_secret = os.getenv("DRIFT_SHARED_SECRET", None)
+
+prometheus_multiproc_dir = os.getenv("prometheus_multiproc_dir", None)
+
+path_prefix = os.getenv("PATH_PREFIX", "/api/")
+
+enable_rbac = str_to_bool(os.getenv("ENABLE_RBAC", "True"))
+
+enable_smart_mgmt_check = str_to_bool(
+    os.getenv("ENABLE_SMART_MANAGEMENT_ENTITLEMENT_CHECK", "False"))
 
 
